d1c923917bc6bcc6b5aa94696c93308b93d15a84
e1an <evanjewell@me.com>
2019-10-21 19:54:29 -0500

    How do we record timings in this implementation?

--- record_input_pyxhook.py
+++ record_input_pyxhook.py
@@ -1,18 +1,23 @@
 import pyxhook
 import time
 
+start_time = end_time = 0 #how tf do we calculate this
+key_timing_array = []
+
 def key_down(event):
     global running
-    print(event.Key, " down")
 
-    if event.Ascii == 13:
+    if event.Ascii == 13: #Return key pressed
         running = False
 
+    print("Pressed ", event.Key, " down")
+    print ("Measured time: ", time.time())
+
 def key_up(event):
-    print(event.Key, " up")
+    print("Pressed ", event.Key, " up")
 
 hookman = pyxhook.HookManager()
-#define callback to fire when key pressed down
+#define callback to fire when key pressed up/down
 hookman.KeyDown = key_down
 hookman.KeyUp = key_up
 hookman.HookKeyboard()
